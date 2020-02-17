8aa6b946db7403825775aa3511ad03b0ee596770
ailtonbsj <ailton.ifce@gmail.com>
2020-02-17 02:00:22 -0300

    Using Route and Schema

--- course-laravel-4.2-gradisteanu/README.md
+++ course-laravel-4.2-gradisteanu/README.md
@@ -11,7 +11,17 @@
 ```bash
 composer create-project laravel/laravel myfirstapp 4.2 --prefer-dist
 
+cd ./myfirstapp
+
 php artisan serve
 
 # edit ./app/config/app.php to enable debug and disable error reporting
+```
+
+## Commands to use
+
+```bash
+cd ./myfirstapp
+
+composer install
 ```
--- course-laravel-4.2-gradisteanu/myfirstapp/app/routes.php
+++ course-laravel-4.2-gradisteanu/myfirstapp/app/routes.php
@@ -13,5 +13,37 @@
 
 Route::get('/', function()
 {
+	// Create the table books
+	Schema::create('books', function($newTable){
+		$newTable->increments('id');
+		$newTable->string('writer');
+		$newTable->string('title', 500);
+		$newTable->text('description');
+		$newTable->date('published');
+		$newTable->integer('copied');
+		$newTable->timestamps();
+	});
+
+	// Update the table books
+	Schema::table('books', function($newTable){
+		$newTable->boolean('in_store');
+		$newTable->dropColumn('copied');
+	});
+
+
 	return View::make('hello');
+});
+
+Route::get('category', function()
+{
+	return 'Category page content';
+});
+
+Route::get('category/sf', function()
+{
+	return 'Science Fiction page content';
+});
+
+Route::get('category/{parameter}', function($parameter){
+	return $parameter . ' page content';
 });
